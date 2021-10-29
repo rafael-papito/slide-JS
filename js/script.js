5c4b853183739bcab06d78a1884b661bbc8e3ffb
Rafael Porto <rafaelgportof@gmail.com>
2021-10-29 09:46:06 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-alert('ok')
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
